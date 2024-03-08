133a99ac57a857166c2e7878bef43b296e943b4c
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-03-08 18:20:10 +0100

    fix

--- gestionnaire_taches_project/settings.py
+++ gestionnaire_taches_project/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 MIDDLEWARE = [
