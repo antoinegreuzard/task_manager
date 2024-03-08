ce084cbd005ff3bf5576657b1d4277d41f011e2b
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-03-08 18:00:30 +0100

    fix: title not showing

--- gestionnaire_taches_project/settings.py
+++ gestionnaire_taches_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zzpoj^youprt(4%yw^pb+^a@rdf$)@=*gb!zdfi^&jlo^9dc40'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'task-manager-ygmd.onrender.com']
 
 # Application definition
 
